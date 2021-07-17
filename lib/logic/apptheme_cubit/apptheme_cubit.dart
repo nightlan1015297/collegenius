821d15a8dc392434a48f70a36967f446c32a50e9
nightlan1015297 <nightlan1015297@gmail.com>
2021-07-17 12:07:03 +0800

    Update state classe for testing purpose

--- lib/logic/apptheme_cubit/apptheme_cubit.dart
+++ lib/logic/apptheme_cubit/apptheme_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
+
 import 'package:collegenius/constants/enums.dart';
 
 part 'apptheme_state.dart';
--- lib/logic/apptheme_cubit/apptheme_state.dart
+++ lib/logic/apptheme_cubit/apptheme_state.dart
@@ -1,9 +1,12 @@
 part of 'apptheme_cubit.dart';
 
-class AppthemeState {
-  ThemeOption cuurrentOption = ThemeOption.light;
+class AppthemeState extends Equatable {
+  final ThemeOption cuurrentOption;
 
   AppthemeState({
     required this.cuurrentOption,
   });
+
+  @override
+  List<Object?> get props => [this.cuurrentOption];
 }
