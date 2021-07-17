2c35adbd36045bc7bfba7141c82b96c541dc29a5
nightlan1015297 <nightlan1015297@gmail.com>
2021-07-17 22:19:35 +0800

    Add comment of function's information

--- lib/ui/pages/settingpage/setting_tile_builder.dart
+++ lib/ui/pages/settingpage/setting_tile_builder.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+/*
+Setting tile builder build the basic tile in setting seection
+- setting tile includes Icon , title and decoration in tail of tile
+*/
+
 Widget settingTileBilder(
     {required IconData icon,
     required String title,
