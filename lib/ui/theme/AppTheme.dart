d3f2358e6e5a87004760c5598461c81a10e3a291
nightlan1015297 <nightlan1015297@gmail.com>
2021-07-17 22:21:51 +0800

    add theme setting

--- lib/ui/theme/AppTheme.dart
+++ lib/ui/theme/AppTheme.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData get light {
     return ThemeData(
-      primaryColor: Colors.blue,
-      backgroundColor: Colors.black,
-      scaffoldBackgroundColor: Colors.purple,
-    );
+        primaryColor: Colors.blue,
+        backgroundColor: Colors.white,
+        scaffoldBackgroundColor: Colors.grey.shade200,
+        textTheme: TextTheme(
+          headline1: TextStyle(color: Colors.black),
+          headline2: TextStyle(color: Colors.black),
+          headline3: TextStyle(color: Colors.black),
+          headline4: TextStyle(color: Colors.black),
+          headline5: TextStyle(color: Colors.black),
+          bodyText1: TextStyle(color: Colors.black),
+          bodyText2: TextStyle(color: Colors.black),
+        ));
   }
 
   static ThemeData get night {
