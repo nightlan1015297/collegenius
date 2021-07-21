c339622db0be550c2e8fd63892e9207a86186149
nightlan1015297 <nightlan1015297@gmail.com>
2021-07-21 13:37:55 +0800

    remove testing prrint

--- lib/ui/pages/settingpage/SettingPage.dart
+++ lib/ui/pages/settingpage/SettingPage.dart
@@ -1,6 +1,7 @@
 import 'package:collegenius/logic/apptheme_cubit/apptheme_cubit.dart';
 import 'package:collegenius/ui/pages/settingpage/setting_section_builder.dart';
 import 'package:collegenius/ui/pages/settingpage/basic_setting_tile_widget.dart';
+import 'package:collegenius/ui/pages/settingpage/setting_tile_theme_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -16,71 +17,83 @@ class SettingPageState extends State<SettingPage> {
   final title = "設定";
   @override
   Widget build(BuildContext context) {
-    print('SettingPagebuild');
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
         title: Text(title),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SettingSectionWidget(sectionname: "Preference", tiles: <Widget>[
-              BlocBuilder<AppthemeCubit, AppthemeState>(
-                builder: (context, state) {
-                  return OptionalSettingTileWidget(
-                    debugprint: "1",
-                    icon: Icons.mode,
-                    currentoption:
-                        state.darkTheme == true ? "Dark Theme" : "Light Theme",
-                    title: "Theme",
-                    ontap: () {
-                      state.darkTheme == true
-                          ? context.read<AppthemeCubit>().changeToLightTheme()
-                          : context.read<AppthemeCubit>().changeToDarkTheme();
-                    },
-                  );
-                },
-              )
-            ]),
-            SettingSectionWidget(
-              sectionname: "Example",
-              tiles: <Widget>[
-                OptionalSettingTileWidget(
-                    debugprint: "2",
-                    icon: Icons.accessible_sharp,
-                    title: "Test",
-                    currentoption: "Test",
-                    ontap: () {}),
-                OptionalSettingTileWidget(
-                    debugprint: "3",
-                    icon: Icons.language,
-                    title: "language",
-                    currentoption: "language",
-                    ontap: () {}),
-                OptionalSettingTileWidget(
-                    debugprint: "4",
-                    icon: Icons.dark_mode,
-                    title: "theme",
-                    currentoption: "theme",
-                    ontap: () {}),
-                IoSettingTileWidget(
-                    icon: Icons.mode,
-                    title: "mode",
-                    value: true,
-                    ontap: (bool) {}),
-                IoSettingTileWidget(
-                    icon: Icons.accessible_sharp,
-                    title: "Test",
-                    value: true,
-                    ontap: (bool) {}),
-                BasicSettingTileWidget(
-                    icon: Icons.language, title: "language", ontap: () {}),
-                BasicSettingTileWidget(
-                    icon: Icons.dark_mode, title: "theme", ontap: () {}),
-              ],
-            ),
-            const SizedBox(height: 10.0),
-          ],
+      body: SettingTileThemeWidget(
+        child: Builder(
+          builder: (context) {
+            final _themeState = context.watch<AppthemeCubit>().state;
+
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  SettingSectionWidget(
+                      sectionname: "Preference",
+                      tiles: <Widget>[
+                        OptionalSettingTileWidget(
+                          icon: Icons.mode,
+                          currentOption: _themeState.darkTheme == true
+                              ? "Dark Theme"
+                              : "Light Theme",
+                          title: "Theme",
+                          ontap: () {
+                            _themeState.darkTheme == true
+                                ? context
+                                    .read<AppthemeCubit>()
+                                    .changeToLightTheme()
+                                : context
+                                    .read<AppthemeCubit>()
+                                    .changeToDarkTheme();
+                          },
+                        ),
+                      ]),
+                  SettingSectionWidget(
+                    sectionname: "Example",
+                    tiles: <Widget>[
+                      OptionalSettingTileWidget(
+                          icon: Icons.accessible_sharp,
+                          title: "Test",
+                          currentOption: "Test",
+                          ontap: () {}),
+                      OptionalSettingTileWidget(
+                          icon: Icons.language,
+                          title: "Language",
+                          currentOption: "language",
+                          ontap: () {}),
+                      OptionalSettingTileWidget(
+                          icon: Icons.dark_mode,
+                          title: "Theme",
+                          currentOption: "theme",
+                          ontap: () {}),
+                      IoSettingTileWidget(
+                          icon: Icons.mode,
+                          title: "Mode",
+                          value: true,
+                          ontap: (bool) {}),
+                      IoSettingTileWidget(
+                          icon: Icons.accessible_sharp,
+                          title: "Test",
+                          value: true,
+                          ontap: (bool) {}),
+                      BasicSettingTileWidget(
+                          icon: Icons.language,
+                          title: "Language",
+                          ontap: () {}),
+                      BasicSettingTileWidget(
+                          icon: Icons.dark_mode, title: "Theme", ontap: () {}),
+                    ],
+                  ),
+                  const SizedBox(height: 10.0),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
