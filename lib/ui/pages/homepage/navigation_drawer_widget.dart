e8eb8b1df77cbc4b3059b9da87455322060dfc5f
nightlan1015297 <nightlan1015297@gmail.com>
2021-07-21 13:39:07 +0800

    Remove anti pattern code

--- lib/ui/pages/homepage/navigation_drawer_widget.dart
+++ lib/ui/pages/homepage/navigation_drawer_widget.dart
@@ -1,31 +1,30 @@
+import 'package:collegenius/ui/pages/homepage/drawer_tile_widget.dart';
 import 'package:flutter/material.dart';
 
 class NavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Material(
-        color: Color.fromRGBO(50, 75, 105, 1),
-        child: ListView(
-          children: <Widget>[
-            buildItem(text: "People", icon: Icons.people),
-          ],
-        ),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                DrawerTile(
+                  icon: Icons.people,
+                  title: "People",
+                  onTap: () {},
+                ),
+                DrawerTile(
+                  icon: Icons.settings,
+                  title: "Setting",
+                  onTap: () => Navigator.of(context).pushNamed("/setting"),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
-}
-
-Widget buildItem({required String text, required IconData icon}) {
-  final color = Colors.white;
-  final hoverColor = Colors.white70;
-  return ListTile(
-    leading: Icon(icon, color: color),
-    title: Text(
-      text,
-      style: TextStyle(color: color),
-    ),
-    hoverColor: hoverColor,
-    onTap: () {},
-  );
 }
