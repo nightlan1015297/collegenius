7fa850b9304d328ab5b00fd0d96bacd3f5c4848b
nightlan1015297 <nightlan1015297@gmail.com>
2021-07-26 16:40:55 +0800

    update

--- lib/ui/routes/Routes.dart
+++ lib/ui/routes/Routes.dart
@@ -1,12 +1,18 @@
-import 'package:collegenius/ui/pages/homepage/Homepage.dart';
-import 'package:collegenius/ui/pages/settingpage/SettingPage.dart';
+import 'package:collegenius/ui/pages/homepage/HomePageBody.dart';
+import 'package:collegenius/ui/pages/main_scaffold/MainScaffold.dart';
+import 'package:collegenius/ui/pages/settingpage/SettingPageScaffold.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings routsettings) {
     switch (routsettings.name) {
       case '/':
-        return MaterialPageRoute(builder: (_) => HomePage());
+        return MaterialPageRoute(
+          builder: (_) => MainScaffold(
+            title: 'HomePage',
+            body: HomePageBody(),
+          ),
+        );
       case '/setting':
         return MaterialPageRoute(builder: (_) => SettingPage());
       default:
