318a292eeae62b1fc1a7ca3962f02d29de41c793
nightlan1015297 <nightlan1015297@gmail.com>
2021-07-26 16:58:20 +0800

    extract Homapage body

---
+++ lib/ui/pages/homepage/HomePageBody.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange,
+      child: Text("HomePage"),
+    );
+  }
+}
