17f50304d3f6a871f2c0d35e48b823e5220a5a5e
nightlan1015297 <nightlan1015297@gmail.com>
2021-09-04 21:24:24 +0800

    add bottom navigation bar

--- lib/ui/pages/main_scaffold/MainScaffold.dart
+++ lib/ui/pages/main_scaffold/MainScaffold.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:collegenius/logic/cubit/bottomnav_cubit.dart';
 
 import 'drawer_tile_widget.dart';
 import 'navigation_drawer_widget.dart';
@@ -14,82 +17,72 @@ class MainScaffold extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      drawer: NavigationDrawer(
-        tiles: [
-          DrawerTile(
-            icon: Icons.people,
-            title: "People",
-            onTap: () {},
-          ),
-          DrawerTile(
-            icon: Icons.settings,
-            title: "Setting",
-            onTap: () => Navigator.of(context).pushNamed("/setting"),
-          ),
-        ],
-      ),
-      body: body,
-      bottomNavigationBar: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(),
-              padding: EdgeInsets.all(10),
-              child: IconButton(
-                icon: Icon(
-                  Icons.home_outlined,
-                  size: 30,
-                ),
-                onPressed: () {},
-              ),
+        appBar: AppBar(
+          title: Text(title),
+          iconTheme: _theme.iconTheme,
+        ),
+        drawer: NavigationDrawer(
+          tiles: [
+            DrawerTile(
+              icon: Icons.people,
+              title: "People",
+              onTap: () {},
             ),
-            Container(
-              margin: EdgeInsets.symmetric(),
-              padding: EdgeInsets.all(10),
-              child: IconButton(
-                icon: Icon(
-                  Icons.directions_bus_outlined,
-                  size: 30,
-                ),
-                onPressed: () {},
-              ),
+            DrawerTile(
+              icon: Icons.settings,
+              title: "Setting",
+              onTap: () => Navigator.of(context).pushNamed("/setting"),
             ),
-            Container(
-              margin: EdgeInsets.symmetric(),
-              padding: EdgeInsets.all(10),
-              child: IconButton(
-                icon: Icon(
-                  Icons.event_note_outlined,
-                  size: 30,
+          ],
+        ),
+        body: body,
+        bottomNavigationBar: Builder(
+          builder: (context) {
+            final _bottomnavState = context.watch<BottomnavCubit>().state;
+
+            return BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              currentIndex: _bottomnavState.index,
+              backgroundColor: _theme.backgroundColor,
+              unselectedIconTheme: _theme.iconTheme,
+              selectedIconTheme: IconThemeData(color: Colors.blue),
+              selectedFontSize: 12,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              onTap: (index) {
+                context.read<BottomnavCubit>().changeIndex(index);
+              },
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined),
+                  activeIcon: Icon(Icons.home),
+                  label: "Home",
                 ),
-                onPressed: () {},
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(),
-              padding: EdgeInsets.all(10),
-              child: IconButton(
-                icon: Icon(
-                  Icons.home,
-                  size: 30,
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: "asd",
                 ),
-                onPressed: () {},
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(),
-              padding: EdgeInsets.all(10),
-              child: IconButton(
-                icon: Icon(
-                  Icons.home,
-                  size: 30,
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: "asd",
                 ),
-                onPressed: () {},
-              ),
-            ),
-          ]),
-    );
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.business),
+                  label: "asd",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.school),
+                  label: "asd",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  label: "asd",
+                ),
+              ],
+            );
+          },
+        ));
   }
 }
