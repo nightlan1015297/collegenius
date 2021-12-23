368d507f61b4c73cf652f90700456c21a5f459b3
nightlan1015297 <nightlan1015297@gmail.com>
2021-12-23 23:19:25 +0800

    Using new approach so delete the lagacy code

--- lib/logic/stream/time_stream.dart
+++
@@ -1,7 +0,0 @@
-class TimeStream {
-  const TimeStream();
-
-  Stream<DateTime> start() {
-    return Stream.periodic(Duration(seconds: 1), (x) => DateTime.now());
-  }
-}
--- lib/model/course_schedual_model.dart
+++
@@ -1,9 +0,0 @@
-class Location {
-  const Location({
-    required this.title,
-    required this.woeid,
-  });
-
-  final String title;
-  final int woeid;
-}
