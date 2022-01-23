fb2d5c924c3acac083ffaa70401d1d409f4c7a5f
nightlan1015297 <nightlan1015297@gmail.com>
2022-01-24 01:04:48 +0800

    implement business logic layer for school event

---
+++ lib/logic/cubit/school_events_cubit.dart
@@ -0,0 +1,46 @@
+import 'package:bloc/bloc.dart';
+import 'package:collegenius/models/event_model/event_model.dart';
+import 'package:equatable/equatable.dart';
+import 'package:school_events_repository/school_events_repository.dart';
+
+part 'school_events_state.dart';
+
+class SchoolEventsCubit extends Cubit<SchoolEventsState> {
+  SchoolEventsCubit(
+    this._courseSchedualRepository,
+  ) : super(SchoolEventsState());
+
+  final SchoolEventsRepository _courseSchedualRepository;
+
+  Future<void> fetchInitEvents() async {
+    emit(state.copywith(status: SchoolEventsStatus.loading));
+    try {
+      var fetchedList = await _courseSchedualRepository.getEvents(1);
+      emit(state.copywith(
+          status: SchoolEventsStatus.success,
+          events: fetchedList.map((e) => Event.fromJson(e)).toList(),
+          loadedPage: 1));
+    } catch (error) {
+      emit(state.copywith(status: SchoolEventsStatus.failure));
+    }
+  }
+
+  Future<void> fetchMoreEvents() async {
+    try {
+      var fetched =
+          await _courseSchedualRepository.getEvents(state.loadedPage + 1);
+      var fetchedList = fetched.map((e) => Event.fromJson(e)).toList();
+
+      if (fetchedList.last == state.events.last) {
+        emit(state.copywith(status: SchoolEventsStatus.loadedend));
+      } else {
+        emit(state.copywith(
+            status: SchoolEventsStatus.success,
+            events: state.events + fetchedList,
+            loadedPage: state.loadedPage + 1));
+      }
+    } catch (error) {
+      emit(state.copywith(status: SchoolEventsStatus.failure));
+    }
+  }
+}
---
+++ lib/logic/cubit/school_events_state.dart
@@ -0,0 +1,35 @@
+part of 'school_events_cubit.dart';
+
+enum SchoolEventsStatus { initial, loading, loadedend, success, failure }
+
+extension SchoolEventsStatusX on SchoolEventsStatus {
+  bool get isInitial => this == SchoolEventsStatus.initial;
+  bool get isLoading => this == SchoolEventsStatus.loading;
+  bool get isLoadmore => this == SchoolEventsStatus.loadedend;
+  bool get isSuccess => this == SchoolEventsStatus.success;
+  bool get isFailure => this == SchoolEventsStatus.failure;
+}
+
+class SchoolEventsState extends Equatable {
+  SchoolEventsState({
+    this.status = SchoolEventsStatus.initial,
+    List<Event>? events,
+    int? loadedPage,
+  })  : loadedPage = loadedPage ?? 1,
+        events = events ?? [];
+
+  final SchoolEventsStatus status;
+  final int loadedPage;
+  final List<Event> events;
+
+  SchoolEventsState copywith(
+      {SchoolEventsStatus? status, int? loadedPage, List<Event>? events}) {
+    return SchoolEventsState(
+        status: status ?? this.status,
+        loadedPage: loadedPage ?? this.loadedPage,
+        events: events ?? this.events);
+  }
+
+  @override
+  List<Object> get props => [status, loadedPage, events];
+}
