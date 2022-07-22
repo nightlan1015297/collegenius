412615148381485a5ea3bdc0bf91c77cf1f2fcd9
nightlan1015297 <nightlan1015297@gmail.com>
2022-07-23 00:58:11 +0800

    remove unused import

--- lib/logic/bloc/authentication_bloc.dart
+++ lib/logic/bloc/authentication_bloc.dart
@@ -1,28 +1,38 @@
 import 'package:bloc/bloc.dart';
 import 'package:collegenius/models/user_model/user_model.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 part 'authentication_event.dart';
 part 'authentication_state.dart';
 
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
-  AuthenticationBloc() : super(const AuthenticationState.unknown()) {
-    on<AuthenticatedRequested>(_onAuthenticatedRequested);
+  AuthenticationBloc() : super(const AuthenticationState.unAuthenticated()) {
+    on<CourseSelectAuthenticatedRequested>(
+        _onCourseSelectAuthenticatedRequested);
+    on<EeclassAuthenticatedRequested>(_onEeclassAuthenticatedRequested);
     on<AuthenticationLogoutRequested>(_onAuthenticationLogoutRequested);
   }
 
-  void _onAuthenticatedRequested(
-    AuthenticatedRequested event,
+  void _onCourseSelectAuthenticatedRequested(
+    CourseSelectAuthenticatedRequested event,
     Emitter<AuthenticationState> emit,
   ) {
-    return emit(AuthenticationState.authenticated(event.user));
+    return emit(AuthenticationState.courseSelectAuthenticated(event.user));
+  }
+
+  void _onEeclassAuthenticatedRequested(
+    EeclassAuthenticatedRequested event,
+    Emitter<AuthenticationState> emit,
+  ) {
+    return emit(AuthenticationState.eeclassAuthenticated(event.user));
   }
 
   void _onAuthenticationLogoutRequested(
     AuthenticationLogoutRequested event,
     Emitter<AuthenticationState> emit,
   ) {
-    return emit(AuthenticationState.unauthenticated());
+    return emit(AuthenticationState.unAuthenticated());
   }
 }
