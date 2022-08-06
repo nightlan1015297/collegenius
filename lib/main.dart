504bdd3c55ac3114bcbd2bcc1c31aaec18670405
nightlan1015297 <nightlan1015297@gmail.com>
2022-08-06 21:40:09 +0800

    add required initilize

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:collegenius/logic/bloc/authentication_bloc.dart';
 import 'package:collegenius/logic/bloc/login_page_bloc.dart';
 import 'package:collegenius/models/course_schedual_model/course_schedual_models.dart';
 import 'package:collegenius/models/semester_model/semester_model.dart';
 import 'package:collegenius/repositories/authtication_repository.dart';
+import 'package:collegenius/repositories/eeclass_repository.dart';
 import 'package:collegenius/ui/pages/course_schedual_page/CourseSchedualView.dart';
 import 'package:collegenius/ui/pages/eeclass_page/EEclassHomePageView.dart';
+import 'package:collegenius/ui/pages/eeclass_page/EeclassCoursePageView.dart';
+import 'package:collegenius/utilties/AppBlocObserver.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
@@ -17,11 +23,10 @@ import 'package:collegenius/ui/main_scaffold/MainScaffold.dart';
 import 'package:collegenius/ui/pages/bulletin_page/BulletinPage.dart';
 import 'package:collegenius/ui/pages/course_schedual_page/CourseSchedualPage.dart';
 import 'package:collegenius/ui/theme/AppTheme.dart';
-import 'package:collegenius/utilties/ticker.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'logic/cubit/apptheme_cubit.dart';
 import 'logic/cubit/bottomnav_cubit.dart';
-import 'logic/cubit/course_schedual_page_cubit.dart';
 import 'logic/cubit/school_events_cubit.dart';
 import 'repositories/course_schedual_repository.dart';
 import 'repositories/school_events_repository.dart';
@@ -32,6 +37,16 @@ import 'routes/Routes.dart';
 Future<void> main() async {
   /* ensureInitialized to prevent unpredictable error*/
   WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+  await Permission.storage.request();
+  await FlutterDownloader.initialize(
+      debug:
+          true, // optional: set to false to disable printing logs to console (default: true)
+      ignoreSsl:
+          true // option: set to false to disable working with http links (default: false)
+      );
 
   /* Using Hive to storage application data*/
   await Hive.initFlutter();
@@ -44,126 +59,129 @@ Future<void> main() async {
   final storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
-  HydratedBlocOverrides.runZoned(
-    () => runApp(MyApp()),
-    storage: storage,
-  );
+  HydratedBlocOverrides.runZoned(() => runApp(MyApp()),
+      storage: storage, blocObserver: AppBlocObserver());
 }
 
 class MyApp extends StatelessWidget {
   final AppRouter _appRouter = AppRouter();
-  final CourseSchedualRepository courseSchedualRepository =
-      CourseSchedualRepository();
-  final SchoolEventsRepository schoolEventsRepository =
-      SchoolEventsRepository();
-  final AuthenticationRepository authenticationRepository =
-      AuthenticationRepository();
+
   @override
   Widget build(BuildContext context) {
     final _theme = Theme.of(context);
-    final _courseSchedualRepository = CourseSchedualRepository();
-    return MultiBlocProvider(
+    return MultiRepositoryProvider(
       providers: [
-        // BlocProvider<AuthenticateCubit>(
-        //   create: (BuildContext context) => AuthenticateCubit(),
-        // ),
-        BlocProvider<AuthenticationBloc>(
-          create: (BuildContext context) => AuthenticationBloc(),
+        RepositoryProvider(
+          create: (BuildContext context) => EeclassRepository(),
         ),
-        BlocProvider<SchoolEventsCubit>(
-          create: (BuildContext context) =>
-              SchoolEventsCubit(schoolEventsRepository),
+        RepositoryProvider(
+          create: (BuildContext context) => CourseSchedualRepository(),
         ),
-        BlocProvider<AppthemeCubit>(
-          create: (BuildContext context) => AppthemeCubit(),
+        RepositoryProvider(
+          create: (BuildContext context) => SchoolEventsRepository(),
         ),
-        BlocProvider<BottomnavCubit>(
-          create: (BuildContext context) => BottomnavCubit(),
-        ),
-        BlocProvider<CourseSchedualPageCubit>(
-            lazy: false,
-            create: (BuildContext context) => CourseSchedualPageCubit(
-                ticker: Ticker(),
-                courseSchedualRepository: _courseSchedualRepository,
-                authenticateBloc: context.read<AuthenticationBloc>())),
-        BlocProvider<LoginPageBloc>(
-          create: (BuildContext context) => LoginPageBloc(
-              authenticationRepository: authenticationRepository,
-              authenticationBloc: context.read<AuthenticationBloc>()),
+        RepositoryProvider(
+          create: (BuildContext context) => AuthenticationRepository(),
         ),
       ],
-      child: Builder(
-        builder: (context) {
-          final _themeState = context.watch<AppthemeCubit>().state;
-          final _bottomNavState = context.watch<BottomnavCubit>().state;
-          final _body;
-          switch (_bottomNavState.index) {
-            case 0:
-              _body = MainScaffold(
-                title: 'Home',
-                body: HomePageView(),
-              );
-              break;
-            case 1:
-              _body = MainScaffold(
-                title: 'Coueses',
-                body: CourseSchedualView(),
-              );
-              break;
-            case 2:
-              _body = MainScaffold(
-                title: 'Bulletins',
-                body: BulletinHomePageView(),
-              );
-              break;
-            case 3:
-              _body = MainScaffold(
-                title: 'Bulletins',
-                body: BulletinHomePageView(),
-              );
-              break;
-            case 4:
-              _body = MainScaffold(
-                title: 'EEclass',
-                body: EeclassHomePageView(),
-              );
-              break;
-            case 5:
-              _body = MainScaffold(
-                title: 'Setting',
-                body: SettingPageView(),
-              );
-              break;
-            default:
-              _body = Center(
-                child: Text(
-                    'Route Error : No route defined for bottomNav index ${_bottomNavState.index}'),
-              );
-          }
-          final _themeMode;
-          switch (_themeState.themeOption) {
-            case AppthemeOption.dark:
-              _themeMode = ThemeMode.light;
-              break;
-            case AppthemeOption.light:
-              _themeMode = ThemeMode.dark;
-              break;
-            case AppthemeOption.system:
-              _themeMode = ThemeMode.system;
-              break;
-          }
-          return MaterialApp(
-            title: 'Collegenius',
-            theme: AppTheme.light,
-            darkTheme: AppTheme.dark,
-            themeMode: _themeMode,
-            onGenerateRoute: _appRouter.generateRoute,
-            home: IconTheme(
-              data: _theme.iconTheme,
-              child: _body,
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthenticationBloc>(
+            create: (BuildContext context) => AuthenticationBloc(
+                eeclassRepository: context.read<EeclassRepository>(),
+                courseSchedualRepository:
+                    context.read<CourseSchedualRepository>()),
+          ),
+          BlocProvider<SchoolEventsCubit>(
+            create: (BuildContext context) => SchoolEventsCubit(
+              schoolEventsRepository: context.read<SchoolEventsRepository>(),
             ),
-          );
-        },
+          ),
+          BlocProvider<AppthemeCubit>(
+            create: (BuildContext context) => AppthemeCubit(),
+          ),
+          BlocProvider<BottomnavCubit>(
+            create: (BuildContext context) => BottomnavCubit(),
+          ),
+          BlocProvider<LoginPageBloc>(
+            create: (BuildContext context) => LoginPageBloc(
+                authenticationRepository:
+                    context.read<AuthenticationRepository>(),
+                authenticationBloc: context.read<AuthenticationBloc>()),
+          ),
+        ],
+        child: Builder(
+          builder: (context) {
+            final _themeState = context.watch<AppthemeCubit>().state;
+            final _themeMode;
+            switch (_themeState.themeOption) {
+              case AppthemeOption.dark:
+                _themeMode = ThemeMode.light;
+                break;
+              case AppthemeOption.light:
+                _themeMode = ThemeMode.dark;
+                break;
+              case AppthemeOption.system:
+                _themeMode = ThemeMode.system;
+                break;
+            }
+            return MaterialApp(
+              title: 'Collegenius',
+              theme: AppTheme.light,
+              darkTheme: AppTheme.dark,
+              themeMode: _themeMode,
+              onGenerateRoute: _appRouter.generateRoute,
+              home: IconTheme(
+                data: _theme.iconTheme,
+                child: Builder(builder: (context) {
+                  final _bottomNavState = context.watch<BottomnavCubit>().state;
+                  switch (_bottomNavState.index) {
+                    case 0:
+                      return MainScaffold(
+                        title: 'Home',
+                        body: HomePageView(),
+                      );
+                    case 1:
+                      return MainScaffold(
+                        title: 'Coueses',
+                        body: CourseSchedualView(
+                          courseSchedualRepository:
+                              context.read<CourseSchedualRepository>(),
+                        ),
+                      );
+                    case 2:
+                      return MainScaffold(
+                        title: 'Bulletins',
+                        body: BulletinHomePageView(),
+                      );
+                    case 3:
+                      return MainScaffold(
+                        title: 'Bulletins',
+                        body: EeclassCoursePageView(
+                          courseSerial: "10501",
+                        ),
+                      );
+                    case 4:
+                      return MainScaffold(
+                        title: 'EEclass',
+                        body: EeclassHomePageView(),
+                      );
+                    case 5:
+                      return MainScaffold(
+                        title: 'Setting',
+                        body: SettingPageView(),
+                      );
+                    default:
+                      return Center(
+                        child: Text(
+                            'Route Error : No route defined for bottomNav index ${_bottomNavState.index}'),
+                      );
+                  }
+                }),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
