5a7f011dd8c359702c11018b12ef103e9cfaa1af
nightlan1015297 <nightlan1015297@gmail.com>
2022-08-06 21:46:03 +0800

    implement eeclass model

---
+++ lib/models/eeclass_model/EeclassBullitinBrief.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'EeclassBullitinBrief.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+EeclassBullitinBrief _$EeclassBullitinBriefFromJson(
+        Map<String, dynamic> json) =>
+    EeclassBullitinBrief(
+      title: json['title'] as String,
+      url: json['url'] as String,
+      readCount: json['readCount'] as String,
+      auther: json['auther'] as String,
+      date: json['date'] as String,
+    );
+
+Map<String, dynamic> _$EeclassBullitinBriefToJson(
+        EeclassBullitinBrief instance) =>
+    <String, dynamic>{
+      'title': instance.title,
+      'url': instance.url,
+      'readCount': instance.readCount,
+      'auther': instance.auther,
+      'date': instance.date,
+    };
