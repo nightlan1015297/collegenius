9553d01342aa53c6da031808a89037bec7ac78fe
nightlan1015297 <nightlan1015297@gmail.com>
2022-08-11 11:10:52 +0800

    forst commit

---
+++ lib/logic/cubit/eeclass_material_detail_cubit.dart
@@ -0,0 +1,35 @@
+import 'package:bloc/bloc.dart';
+import 'package:collegenius/models/eeclass_model/EeclassModel.dart';
+import 'package:collegenius/repositories/eeclass_repository.dart';
+import 'package:collegenius/utilties/ColorfulPrintFunction.dart';
+import 'package:equatable/equatable.dart';
+
+part 'eeclass_material_detail_state.dart';
+
+class EeclassMaterialDetailCubit extends Cubit<EeclassMaterialDetailState> {
+  EeclassMaterialDetailCubit({
+    required this.eeclassRepository,
+  }) : super(EeclassMaterialDetailState());
+  final EeclassRepository eeclassRepository;
+
+  Future<void> onOpenPopupMaterialCardRequest({
+    required String type,
+    required String materialUrl,
+  }) async {
+    emit(state.copyWith(
+        detailCardStatus: EeclassMaterialDetailCardStatus.loading));
+    try {
+      final materialInfo =
+          await eeclassRepository.getMaterial(type: type, url: materialUrl);
+      emit(state.copyWith(
+          detailCardStatus: EeclassMaterialDetailCardStatus.success,
+          materialCardData: materialInfo));
+    } catch (e, stacktrace) {
+      print(e);
+      printHighlight(stacktrace);
+
+      emit(state.copyWith(
+          detailCardStatus: EeclassMaterialDetailCardStatus.failed));
+    }
+  }
+}
---
+++ lib/logic/cubit/eeclass_material_detail_state.dart
@@ -0,0 +1,35 @@
+part of 'eeclass_material_detail_cubit.dart';
+
+enum EeclassMaterialDetailCardStatus {
+  loading,
+  success,
+  failed,
+  unauthenticate,
+}
+
+class EeclassMaterialDetailState extends Equatable {
+  const EeclassMaterialDetailState({
+    EeclassMaterialDetailCardStatus? detailCardStatus,
+    EeclassMaterial? materialCardData,
+  })  : detailCardStatus =
+            detailCardStatus ?? EeclassMaterialDetailCardStatus.loading,
+        materialCardData = materialCardData;
+  final EeclassMaterialDetailCardStatus detailCardStatus;
+  final EeclassMaterial? materialCardData;
+
+  EeclassMaterialDetailState copyWith({
+    EeclassMaterialDetailCardStatus? detailCardStatus,
+    EeclassMaterial? materialCardData,
+  }) {
+    return EeclassMaterialDetailState(
+      detailCardStatus: detailCardStatus ?? this.detailCardStatus,
+      materialCardData: materialCardData ?? this.materialCardData,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        detailCardStatus,
+        materialCardData,
+      ];
+}
