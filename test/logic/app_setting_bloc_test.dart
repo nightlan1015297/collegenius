bad17dad30f8e2f671a3dceef0e028e0e3614676
nightlan1015297 <nightlan1015297@gmail.com>
2022-08-28 02:12:07 +0800

    test

---
+++ test/logic/app_setting_bloc_test.dart
@@ -0,0 +1,20 @@
+// import 'package:bloc_test/bloc_test.dart';
+// import 'package:collegenius/logic/bloc/app_setting_bloc.dart';
+// import 'package:hydrated_bloc/hydrated_bloc.dart';
+// import 'package:test/test.dart';
+// import 'package:mockito/mockito.dart';
+
+// class MockAppSettingBloc extends MockBloc<AppSettingBloc,AppSettingState> implements AppSettingBloc {}
+// void main() {
+  
+//   group('App global setting bloc testing', () {
+//     blocTest(
+//       'Checking the initial state',
+//       build: () => AppSettingBloc(),
+//       expect: () => [AppSettingState()],
+//     );
+//   });
+// }
+
+/// Now the Hydratedbloc related integration test cannot implement
+/// See -> https://github.com/felangel/bloc/issues/3350
---
+++ test/test_tool/hydratedblocBuilder.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:hydrated_bloc/hydrated_bloc.dart';
+// import 'package:mocktail/mocktail.dart';
+
+// class MockStorage extends Mock implements Storage {}
+
+// late Storage hydratedStorage;
+
+// void initHydratedStorage() {
+//   TestWidgetsFlutterBinding.ensureInitialized();
+//   hydratedStorage = MockStorage();
+//   when(
+//     () => hydratedStorage.write(any(), any<dynamic>()),
+//   ).thenAnswer((_) async {});
+//   HydratedBloc.storage = hydratedStorage;
+// }
+
+
+/// Now the Hydratedbloc related integration test cannot implement
+/// See -> https://github.com/felangel/bloc/issues/3350
