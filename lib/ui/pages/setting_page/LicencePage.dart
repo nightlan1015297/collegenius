fc0a8109a0145914ec26b6d76382fa684150e9a2
nightlan1015297 <nightlan1015297@gmail.com>
2022-08-29 21:46:17 +0800

    update version to 1.0.2

--- lib/ui/pages/setting_page/LicencePage.dart
+++ lib/ui/pages/setting_page/LicencePage.dart
@@ -4,12 +4,10 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 class LicencePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final _theme = Theme.of(context);
     return LicensePage(
         applicationName: "Collegenius",
         applicationIcon: ClipRRect(
             borderRadius: BorderRadius.circular(20),
-            child: Image.asset('images/collegenius.png')),
-        applicationVersion: "0.0.1");
+            child: Image.asset('images/collegenius.png')));
   }
 }
