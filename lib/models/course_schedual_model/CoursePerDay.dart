6d0adff2143a65512622418f705c2a82d0f1f953
nightlan1015297 <nightlan1015297@gmail.com>
2022-08-29 21:46:17 +0800

    update version to 1.0.2

--- lib/models/course_schedual_model/CoursePerDay.dart
+++ lib/models/course_schedual_model/CoursePerDay.dart
@@ -1,4 +1,4 @@
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'Course.dart';
 import 'package:equatable/equatable.dart';
