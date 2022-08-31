7e9750932d60664b9ae1f5966d0f381f21334a23
nightlan1015297 <nightlan1015297@gmail.com>
2022-08-31 16:49:45 +0800

    add some useful comment <3

--- lib/ui/common_widgets/IconInformationProvider.dart
+++ lib/ui/common_widgets/IconInformationProvider.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+/// TextInformationProvider provides an labeled text information.
+/// See: https://github.com/nightlan1015297/collegenius/wiki/Icon-Information-Provider
+
 class IconInformationProvider extends StatelessWidget {
   IconInformationProvider({
     Key? key,
@@ -10,17 +13,41 @@ class IconInformationProvider extends StatelessWidget {
     this.labelTextOverFlow = TextOverflow.ellipsis,
     this.labelMaxLines,
   }) : super(key: key);
+
+  /// The text that is going to show on label.
   final String label;
+
+  /// The MainAxisAlignment that wraps label text and information Icon.
+  /// default is [MainAxisAlignment.center]
   final MainAxisAlignment mainAxisAlignment;
+
+  /// Specified the [TextStyle] of label text.
+  /// The default value is [labelLarge], it is better not to use large text
+  /// to label. Such us [headline] level text.
+  /// But overall, it is up to you. If [headline] level text makes perfect.
+  /// Just use it.
   final TextStyle? labelTexttheme;
+
+  /// Specified the Overflow behaview of label text, it is set to
+  /// [TextOverflow.ellipsis] as default to prevent RenderFlexOverFlow.
   final TextOverflow labelTextOverFlow;
+
+  /// Specified the maxline of label text,
+  /// [Null] as default will render only 1 line as max.
+  /// It is better not render more then 1 lines since label is just a
+  /// "short" text that describe what information that information icon is
+  /// going to show.
   final int? labelMaxLines;
+
+  /// The information Icon that is going to provide a useful category
+  /// information or status information to the user.
   final Icon informationIcon;
 
   @override
   Widget build(BuildContext context) {
     final _theme = Theme.of(context);
 
+    /// Columns that wrap label text and information Icons together.
     return Column(
       mainAxisAlignment: mainAxisAlignment,
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -29,6 +56,8 @@ class IconInformationProvider extends StatelessWidget {
             maxLines: labelMaxLines,
             overflow: labelTextOverFlow,
             style: labelTexttheme ?? _theme.textTheme.labelLarge),
+
+        /// Small padding to avoid crowded.
         Padding(
           padding: const EdgeInsets.all(4.0),
           child: informationIcon,
