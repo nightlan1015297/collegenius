4a90a7eb7247df5994e6c26704a363a8eca3c18d
nightlan1015297 <nightlan1015297@gmail.com>
2022-10-06 23:54:25 +0800

    feat: provide more information in CLI from bloc observer

--- lib/utilties/AppBlocObserver.dart
+++ lib/utilties/AppBlocObserver.dart
@@ -7,8 +7,19 @@ class AppBlocObserver extends BlocObserver {
   void onChange(BlocBase<dynamic> instance, Change<dynamic> change) {
     printUnimportant("=======[On change]=======");
     printUnimportant("instance : $instance  state has updated.");
+    // printUnimportant(change.currentState);
+    // printUnimportant(change.nextState);
     printUnimportant("=========================");
     return super.onChange(instance, change);
+  }
+
+  @override
+  void onEvent(Bloc<dynamic, dynamic> instance, Object? event) {
+    printUnimportant("=======[On enent]=======");
+    printUnimportant("instance : $instance  has recieve new event.");
+    printUnimportant("event : $event ");
+    printUnimportant("=========================");
+    return super.onEvent(instance, event);
   }
 
   @override
