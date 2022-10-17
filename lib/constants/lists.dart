a484eaed6b0a8805c55a416aecb9dcb37d1be693
nightlan1015297 <nightlan1015297@gmail.com>
2022-10-17 12:39:31 +0800

    chore: extract list constant for course section

--- lib/constants/lists.dart
+++ lib/constants/lists.dart
@@ -1,1 +1,18 @@
-
+const List<String> courseSections = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'Z',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F'
+];
